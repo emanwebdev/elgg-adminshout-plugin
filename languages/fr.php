fab5fd9ecab49bb719422dcac3a5880b1a73c9e6
emanwebdev <emanwebdev@gmail.com>
2012-05-27 14:53:20 +0200

    French Translation
    + minor minor fixes

---
+++ languages/fr.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Adminshout French language file
+ */
+
+$french = array(
+	'admin:administer_utilities:adminshout' => 'Annonce Email',
+	'adminshout' => 'Envoyer un message à tous les membres',
+	'adminshout:description' => 'Utilisez le formulaire ci-dessous pour envoyer un message à tous les membres de votre site. Notez que cela peut prendre un certain temps.',
+	'adminshout:subject:label' => 'Subjet',
+	'adminshout:message:label' => 'Votre message',
+	'adminshout:success' => 'Message envoyé avec succès aux membres',
+	'adminshout:fail' => 'Impossible de notifier les utilisateurs',
+	'adminshout:inputs' => 'Assurez-vous svp d\'inclure un titre et un contenu au message qui sera envoyé à tous les utilisateurs',
+	'adminshout:send' => 'Envoyer le message'
+);
+
+add_translation("fr", $french);
+?>
---
+++ views/default/admin/administer_utilities/adminshout.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Adminshout admin page
+ */
+
+echo '<p>' . elgg_echo('adminshout:description') . '</p>';
+
+echo elgg_view_form('adminshout/send');
+?>
